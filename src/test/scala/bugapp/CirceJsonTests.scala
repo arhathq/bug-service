1234f6b25eff803ddd2b936648f0e480c736de29
arhathq <arhathq@gmail.com>
2016-09-08 08:36:20 +0300

    refactoring circe tests with scala test

--- src/test/scala/bugapp/CirceJsonTests.scala
+++ src/test/scala/bugapp/CirceJsonTests.scala
@@ -1,14 +1,14 @@
 package bugapp
 
+import org.scalatest.FunSuite
 import bugapp.bugzilla.{BugzillaParams, BugzillaRequest}
+import io.circe.Printer
 
 /**
   * @author Alexander Kuleshov
   */
-object CirceJsonTests extends App {
+class CirceJsonTests extends FunSuite {
   import io.circe.{ Decoder, Encoder, Json => JsonC }
-  import io.circe.generic.semiauto._
-  import io.circe.jawn._
   import io.circe.generic.auto._, io.circe.syntax._
 
   //  val jsonVal = """{"service": "hello", "active": true}"""
@@ -25,31 +25,87 @@ object CirceJsonTests extends App {
   case class Params(username: String, password: Option[String] = None, status: Option[List[String]] = None)
   object Params {
     def apply(username: String): Params = new Params(username)
+    def apply(username: String, password: String): Params = new Params(username, Some(password), None)
+
+    def jsonPrinter(): Printer = Printer(preserveOrder = true, dropNullKeys = true, indent = "")
   }
 
-  val p = Params("user1")
-  println(p.asJson.noSpaces)
+  test("Params test") {
+    val p = Params("user1", "pwd")
+    println(p.asJson.pretty(Params.jsonPrinter()))
+  }
 
-  case class Foo(ab: Option[Int])
-  val p1 = Foo(Some(1))
-  println(p1.asJson.noSpaces)
+  test("Foo") {
+    case class Foo(ab: Option[Int])
+    val p1 = Foo(Some(1))
+    println(p1.asJson.spaces2)
+  }
 
-  //  case class Param(key: String, value: String)
-  //  object Param {
-  //    implicit val encodeFoo: Encoder[Param] = deriveEncoder
-  //  }
+  test("BugzillaParams") {
+    val openBugs = BugzillaParams(
+      "username1",
+      "sadsf444",
+      Some(List("UNCOFIRMED", "NEW", "ASSIGNED", "IN_PROGRESS", "BLOCKED", "PROBLEM_DETERMINED", "REOPENED")),
+      Some(List("P1", "P2")))
+    println(BugzillaParams.toJsonString(openBugs))
+  }
 
-  val openBugs = BugzillaParams(
-    "username1",
-    "sadsf444",
-    Some("2016-01-01")
-  )
-  println(List(openBugs).asJson.noSpaces)
+  test("Implicit encoder") {
+    implicit val encodeStringorList: Encoder[Either[String, List[String]]] =
+      Encoder.instance(_.fold(_.asJson, _.asJson))
+    @inline def encodeC[A](a: A)(implicit encode: Encoder[A]): JsonC = encode(a)
+    println(encodeC(List(Map[String, Either[String, List[String]]]("a" -> Left("b"), "b" -> Left("d"), "d" -> Left("1"), "e" -> Right(List("1", "2"))))))
 
-  implicit val encodeStringorList: Encoder[Either[String, List[String]]] =
-    Encoder.instance(_.fold(_.asJson, _.asJson))
-  @inline def encodeC[A](a: A)(implicit encode: Encoder[A]): JsonC = encode(a)
-  println(encodeC(List(Map[String, Either[String, List[String]]]("a" -> Left("b"), "b" -> Left("d"), "d" -> Left("1"), "e" -> Right(List("1", "2"))))))
+    //  println(Map("a" -> "1", "b" -> "2").asJson.noSpaces)
+  }
 
-  //  println(Map("a" -> "1", "b" -> "2").asJson.noSpaces)
+  test("Iterating through Json") {
+    import io.circe._, io.circe.parser._
+
+    val json: String = """
+      {
+        "id": "c730433b-082c-4984-9d66-855c243266f0",
+        "name": "Foo",
+        "counts": [1, 2, 3],
+        "values": {
+          "bar": true,
+          "baz": 100.001,
+          "qux": ["a", "b"],
+          "result" : {
+            "bugs" : [
+              {"q" : "1"}, {"f" : "2"}, {"z" : "3"}
+            ]
+          }
+        }
+      }"""
+
+    val doc: Json = parse(json).getOrElse(Json.Null)
+
+    val cursor: HCursor = doc.hcursor
+
+    val baz: Decoder.Result[Double] =
+      cursor.downField("values").downField("baz").as[Double]
+    println(baz)
+
+    // You can also use `get[A](key)` as shorthand for `downField(key).as[A]`
+    val baz2: Decoder.Result[Double] =
+    cursor.downField("values").get[Double]("baz")
+
+    val secondQux: Decoder.Result[String] =
+      cursor.downField("values").downField("qux").downArray.right.as[String]
+    println(secondQux)
+
+    val arrayQux: Decoder.Result[Array[String]] =
+      cursor.downField("values").downField("qux").as[Array[String]]
+    println(arrayQux.getOrElse(Array()).length)
+/*
+    val arrayBugs: Decoder.Result[Array[B]] =
+      cursor.downField("values").downField("result").downField("bugs").as[Array[B]]
+    println(arrayBugs.getOrElse(Array[B]()).length)
+
+    println(cursor.downField("values").downField("result").downField("bugs").downArray.as[Array[(String, Int)]])
+
+    case class B(i: Int)
+*/
+  }
 }
