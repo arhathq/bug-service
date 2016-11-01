8591738487cff74bd44250ed8b0242988e2362d1
Alexander Kuleshov <oleksandr.kulieshov@globoforce.com>
2016-11-01 19:49:58 +0300

    dev sla charts

--- src/main/scala/bugapp/Config.scala
+++ src/main/scala/bugapp/Config.scala
@@ -1,13 +1,40 @@
 package bugapp
 
-import akka.actor.Props
-import bugapp.report.AllOpenBugsReportActor
 import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config => TypesafeConfig}
 
 trait Config {
 
   private[bugapp] val config = ConfigFactory.load()
 
+  def propsFromConfig(config: TypesafeConfig): Map[String, Any] = {
+    import scala.collection.JavaConversions._
+    config.entrySet().map(entry => entry.getKey -> entry.getValue.unwrapped())(collection.breakOut)
+  }
+
+  /*
+   * Parses following config to a Map
+   *
+   *  config = {
+   *    key1 = {
+   *      value1 = "v1"
+   *      value2 = "v2"
+   *    }
+   *
+   *    key2 = {
+   *      value1 = "v3"
+   *      value2 = "v4"
+   *    }
+   *  }
+   */
+  def getConfigMap(config: TypesafeConfig, path: String): Map[String, Map[String, Any]] = {
+    import scala.collection.JavaConversions._
+    config.getConfig(path).entrySet().
+      map { entry =>
+        val keys = entry.getKey.split("\\.").toSeq
+        keys.head -> propsFromConfig(entry.getValue.atPath(keys.last))
+      } (collection.breakOut)
+  }
 }
 
 trait HttpConfig extends Config {
@@ -49,5 +76,7 @@ trait ReportConfig extends Config {
   val fopConf = reportConfig.getString("fopConf")
   val reportDir = reportConfig.getString("reportDir")
   val templateDir = reportConfig.getString("templateDir")
+
+  val reportTypes = getConfigMap(reportConfig, "types")
 
 }
