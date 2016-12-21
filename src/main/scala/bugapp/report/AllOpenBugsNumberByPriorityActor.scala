9db2e8673c053b84e7551c65fd0479776a63b6ec
arhathq <arhathq@gmail.com>
2016-12-21 20:33:11 +0300

    dev report

--- src/main/scala/bugapp/report/AllOpenBugsNumberByPriorityActor.scala
+++ src/main/scala/bugapp/report/AllOpenBugsNumberByPriorityActor.scala
@@ -1,9 +1,12 @@
 package bugapp.report
 
+import java.time.OffsetDateTime
+
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import bugapp.bugzilla.Metrics
 import bugapp.report.ReportDataBuilder.{ReportDataRequest, ReportDataResponse}
 import bugapp.repository.Bug
+import org.jfree.data.category.DefaultCategoryDataset
 
 import scala.xml._
 
@@ -18,6 +21,7 @@ class AllOpenBugsNumberByPriorityActor(owner: ActorRef) extends Actor with Actor
   def receive: Receive = {
     case ReportDataRequest(reportId, reportParams, bugs) =>
       val excludedComponents: Seq[String] = reportParams(ReportParams.ExcludedComponents).asInstanceOf[Seq[String]]
+      val endDate = reportParams(ReportParams.EndDate).asInstanceOf[OffsetDateTime]
 
       val openBugs = bugs.filter(bug => bug.stats.status == Metrics.OpenStatus)
       val prioritizedOpenBugs = openBugs.groupBy(bug => bug.priority)
@@ -36,6 +40,10 @@ class AllOpenBugsNumberByPriorityActor(owner: ActorRef) extends Actor with Actor
             { prioritizedBugsElem(Metrics.NPPriority, npOpenBugs) }
             {prioritizedBugsElem("Grand Total", openBugs)}
             <excludedComponents>{excludedComponents.mkString("\'", "\', \'", "\'")}</excludedComponents>
+            <image>
+              <content-type>image/jpeg</content-type>
+              <content-value>{allOpenBugsChart(endDate, p1OpenBugs ++ p2OpenBugs)}</content-value>
+            </image>
           </all-open-bugs>
 
        owner ! ReportDataResponse(reportId, data)
@@ -50,6 +58,25 @@ class AllOpenBugsNumberByPriorityActor(owner: ActorRef) extends Actor with Actor
       <total>{val total = data.map(_._2).sum; if(total > 0) total}</total>
     </prioritized-bugs>
   }
+
+  def allOpenBugsChart(endDate: OffsetDateTime, bugs: Seq[Bug]): String = {
+    val marks = Metrics.marks(endDate, 3)
+    val dataSet = new DefaultCategoryDataset()
+    allOpenBugsChartData(Metrics.P1Priority, marks, bugs.filter(bug => bug.priority == Metrics.P1Priority)).foreach(data => dataSet.addValue(data._1, data._2, data._3))
+    allOpenBugsChartData(Metrics.P2Priority, marks, bugs.filter(bug => bug.priority == Metrics.P2Priority)).foreach(data => dataSet.addValue(data._1, data._2, data._3))
+    ChartGenerator.generateBase64OpenHighPriorityBugs(dataSet)
+  }
+
+  def allOpenBugsChartData(priority: String, marks:Seq[String], bugs: Seq[Bug]): Seq[(Int, String, String)] = {
+    val grouped = bugs.groupBy(bug => bug.stats.openMonth)
+    marks.map { mark =>
+      grouped.get(mark) match {
+        case Some(v) => (v.length, priority, mark)
+        case None => (0, priority, mark)
+      }
+    }
+  }
+
 }
 
 object AllOpenBugsNumberByPriorityActor {
