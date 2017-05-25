d1df3791e88cb5e3d6c50905f4147be72f2fe3d6
Alexander Kuleshov <oleksandr.kulieshov@globoforce.com>
2017-05-25 10:52:06 +0300

    dev report unit tests

--- build.sbt
+++ build.sbt
@@ -20,6 +20,7 @@ libraryDependencies ++= {
     "com.typesafe.akka"      %% "akka-http"                         % akkaHttpVersion,
     "com.typesafe.akka"      %% "akka-slf4j"                        % akkaVersion,
     "com.typesafe.akka"      %% "akka-stream"                       % akkaVersion,
+    "com.typesafe.akka"      %% "akka-testkit"                      % akkaVersion,
 
     "com.enragedginger"      %% "akka-quartz-scheduler"             % "1.6.0-akka-2.4.x",
 
--- src/test/java/bugapp/report/japi/ReportActorTest.java
+++ src/test/java/bugapp/report/japi/ReportActorTest.java
@@ -3,18 +3,25 @@ package bugapp.report.japi;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.actor.UntypedActor;
 import akka.testkit.JavaTestKit;
 import akka.testkit.TestKit;
+import akka.testkit.TestProbe;
 import bugapp.report.ReportDataBuilderActor;
+import bugapp.report.ReportParams;
+import bugapp.report.model.ReportData;
 import bugapp.repository.Bug;
+import bugapp.repository.BugCreatedEvent;
+import bugapp.repository.BugEvent;
 import org.junit.*;
 import scala.collection.JavaConversions;
 import scala.collection.JavaConverters$;
 import scala.concurrent.duration.Duration;
 
+import java.time.OffsetDateTime;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Alexander Kuleshov
@@ -22,22 +29,6 @@ import java.util.concurrent.TimeUnit;
 public class ReportActorTest {
 
     private static ActorSystem system;
-
-    public static class SomeActor extends UntypedActor {
-        ActorRef target = null;
-
-        public void onReceive(Object msg) {
-
-            if (msg.equals("hello")) {
-                getSender().tell("world", getSelf());
-                if (target != null) target.forward(msg, getContext());
-
-            } else if (msg instanceof ActorRef) {
-                target = (ActorRef) msg;
-                getSender().tell("done", getSelf());
-            }
-        }
-    }
 
     @BeforeClass
     public static void setup() {
@@ -53,14 +44,36 @@ public class ReportActorTest {
 
     @Test
     public void testTopAssigneesJActor() {
-
         new JavaTestKit(system) {{
-            final ActorRef owner = system.actorOf(Props.create(SomeActor.class));
+            final TestProbe probe = new TestProbe(system);
 
-            final Props props = TopAssigneesJActor.props(owner);
+            final Props props = TopAssigneesJActor.props(probe.ref());
             final ActorRef reportActor = system.actorOf(props);
 
-            reportActor.tell(ReportActorHelper.createRequest(new ArrayList<>(), new HashMap<>()), owner);
+            List<Bug> bugs = new ArrayList<>();
+            bugs.add(
+                    BugBuilder.createWithId(1).withOpened(OffsetDateTime.now()).
+                            withPriority("P2").withAssignee("user1@domain").build()
+            );
+            bugs.add(
+                    BugBuilder.createWithId(2).withOpened(OffsetDateTime.now()).
+                            withPriority("P2").withAssignee("user2@domain").build()
+            );
+            bugs.add(
+                    BugBuilder.createWithId(3).withOpened(OffsetDateTime.now()).
+                            withPriority("P1").withAssignee("user1@domain").build()
+            );
+
+            reportActor.tell(ReportActorHelper.createRequest(bugs, new HashMap<>()), probe.ref());
+
+            final ReportDataBuilderActor.ReportDataResponse message = probe.expectMsgClass(ReportDataBuilderActor.ReportDataResponse.class);
+
+            assertTrue(message.result() instanceof ReportData);
+
+            final ReportData reportData = (ReportData) message.result();
+
+            assertTrue("top-asignees".equals(reportData.name()));
+
         }};
     }
 
@@ -68,12 +81,54 @@ public class ReportActorTest {
     public void testAllOpenBugsNumberByPriorityJActor() {
 
         new JavaTestKit(system) {{
-            final ActorRef owner = system.actorOf(Props.create(SomeActor.class));
+            final TestProbe probe = new TestProbe(system);
 
-            final Props props = AllOpenBugsNumberByPriorityJActor.props(owner);
+            final Props props = AllOpenBugsNumberByPriorityJActor.props(probe.ref());
             final ActorRef reportActor = system.actorOf(props);
 
-            reportActor.tell(ReportActorHelper.createRequest(new ArrayList<>(), new HashMap<>()), owner);
+            List<Bug> bugs = new ArrayList<>();
+            bugs.add(
+                    BugBuilder.createWithId(1).withOpened(OffsetDateTime.now().minusMonths(24)).
+                            withPriority("P2").withAssignee("user1@domain").build()
+            );
+            bugs.add(
+                    BugBuilder.createWithId(1).withOpened(OffsetDateTime.now().minusMonths(12)).
+                            withPriority("P2").withAssignee("user1@domain").build()
+            );
+            bugs.add(
+                    BugBuilder.createWithId(2).withOpened(OffsetDateTime.now().minusDays(91)).
+                            withPriority("P2").withAssignee("user1@domain").build()
+            );
+            bugs.add(
+                    BugBuilder.createWithId(3).withOpened(OffsetDateTime.now().minusDays(31)).
+                            withPriority("P2").withAssignee("user1@domain").build()
+            );
+            bugs.add(
+                    BugBuilder.createWithId(4).withOpened(OffsetDateTime.now().minusDays(8)).
+                            withPriority("P2").withAssignee("user1@domain").build()
+            );
+            bugs.add(
+                    BugBuilder.createWithId(4).withOpened(OffsetDateTime.now().minusDays(5)).
+                            withPriority("P2").withAssignee("user1@domain").build()
+            );
+            bugs.add(
+                    BugBuilder.createWithId(5).withOpened(OffsetDateTime.now()).
+                            withPriority("P2").withAssignee("user1@domain").build()
+            );
+
+            Map<String, Object> params = new HashMap<>();
+            List<String> excludedComponents = new ArrayList<>();
+            params.put(ReportParams.ExcludedComponents(), JavaConversions.asScalaBuffer(excludedComponents));
+
+            reportActor.tell(ReportActorHelper.createRequest(bugs, params), probe.ref());
+
+            final ReportDataBuilderActor.ReportDataResponse message = probe.expectMsgClass(ReportDataBuilderActor.ReportDataResponse.class);
+
+            assertTrue(message.result() instanceof ReportData);
+
+            final ReportData reportData = (ReportData) message.result();
+
+            assertTrue("all-open-bugs".equals(reportData.name()));
         }};
     }
 
@@ -81,11 +136,133 @@ public class ReportActorTest {
         static ReportDataBuilderActor.ReportDataRequest createRequest(final List<Bug> bugs, final Map<String, Object> reportParams) {
             final String requestId = UUID.randomUUID().toString();
             return new ReportDataBuilderActor.ReportDataRequest(requestId,
-            JavaConverters$.MODULE$.mapAsScalaMapConverter(reportParams).asScala().toMap(
-                    scala.Predef$.MODULE$.<scala.Tuple2<String, Object>>conforms()
-            ),
+                    JavaConverters$.MODULE$.mapAsScalaMapConverter(reportParams).asScala().toMap(
+                            scala.Predef$.MODULE$.<scala.Tuple2<String, Object>>conforms()
+                    ),
                     JavaConversions.asScalaBuffer(bugs).toList());
         }
 
+    }
+
+    public static class BugBuilder {
+        private int id;
+        private String severity;
+        private String priority;
+        private String status;
+        private String resolution;
+        private String reporter;
+        private OffsetDateTime opened;
+        private String assignee;
+        private OffsetDateTime changed;
+        private String product;
+        private String component;
+        private String environment;
+        private String summary;
+        private String hardware;
+        private List<BugEvent> events = new ArrayList<>();
+
+        private BugBuilder(int id) {
+            this.id = id;
+        }
+
+        static BugBuilder createWithId(int id) {
+            return new BugBuilder(id);
+        }
+
+        public BugBuilder withSeverity(String severity) {
+            this.severity = severity;
+            return this;
+        }
+
+        public BugBuilder withPriority(String priority) {
+            this.priority = priority;
+            return this;
+        }
+
+        public BugBuilder withStatus(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public BugBuilder withResolution(String resolution) {
+            this.resolution = resolution;
+            return this;
+        }
+
+        public BugBuilder withReporter(String reporter) {
+            this.reporter = reporter;
+            return this;
+        }
+
+        public BugBuilder withOpened(OffsetDateTime opened) {
+            this.opened = opened;
+            return this;
+        }
+
+        public BugBuilder withAssignee(String assignee) {
+            this.assignee = assignee;
+            return this;
+        }
+
+        public BugBuilder withChanged(OffsetDateTime changed) {
+            this.changed = changed;
+            return this;
+        }
+
+        public BugBuilder withProduct(String product) {
+            this.product = product;
+            return this;
+        }
+
+        public BugBuilder withComponent(String component) {
+            this.component = component;
+            return this;
+        }
+
+        public BugBuilder withEnvironment(String environment) {
+            this.environment = environment;
+            return this;
+        }
+
+        public BugBuilder withSummary(String summary) {
+            this.summary = summary;
+            return this;
+        }
+
+        public BugBuilder withHardware(String hardware) {
+            this.hardware = hardware;
+            return this;
+        }
+
+        public BugBuilder withBugEvent(BugEvent bugEvent) {
+            this.events.add(bugEvent);
+            return this;
+        }
+
+        public BugBuilder withBugEvents(List<BugEvent> bugEvents) {
+            this.events.addAll(bugEvents);
+            return this;
+        }
+
+
+        private void validate() {
+            if (opened == null) {
+                throw new RuntimeException("Filed opened is not defined");
+            }
+
+            if (severity == null) {
+
+            } else if (priority == null) {
+
+            }
+        }
+
+        public Bug build() {
+            validate();
+            events.add(0, new BugCreatedEvent(0, id, opened, reporter));
+            return new Bug(id, severity, priority, status, resolution,
+                    reporter, opened, assignee, changed, product, component,
+                    environment, summary, hardware, JavaConversions.asScalaBuffer(events).toList());
+        }
     }
 }
